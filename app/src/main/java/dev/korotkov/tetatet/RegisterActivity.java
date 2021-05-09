93f9877e612482b846a5f9184dbd430cc866e7f3
Korotkov Ilya <i59korotkov@gmail.com>
2021-05-09 21:28:59 +0300

    Register activity. Loading interests from database

--- app/src/main/java/dev/korotkov/tetatet/RegisterActivity.java
+++ app/src/main/java/dev/korotkov/tetatet/RegisterActivity.java
@@ -1,20 +1,52 @@
 package dev.korotkov.tetatet;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.ThrowOnExtraProperties;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+
+    String userId;
+
     TextView registerAvatarBtn;
+    TextView registerInterestsBtn;
+
+    // Interests map loaded from firebase
+    HashMap<String, String> interests = new HashMap<>();
+    HashMap<String, String> chosenInterests = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +55,15 @@ public class RegisterActivity extends AppCompatActivity {
 
         startBackgroundAnimation();
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        userId = firebaseAuth.getCurrentUser().getUid();
+
         registerAvatarBtn = (TextView) findViewById(R.id.register_avatar_btn);
+        registerInterestsBtn = (TextView) findViewById(R.id.register_interests_btn);
+
+        loadInterestsMapFromDatabase();
 
         registerAvatarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,19 +71,84 @@ public class RegisterActivity extends AppCompatActivity {
                 showDialogAvatarChoice();
             }
         });
+
+        registerInterestsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogInterestsChoice();
+            }
+        });
+    }
+
+    private void loadInterestsMapFromDatabase() {
+        firebaseFirestore.collection("interests").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot document : task.getResult().getDocuments()) {
+                        String interestEmoji = document.get("emoji").toString();
+                        String interestName = document.get("name").toString();
+
+                        interests.put(document.getId(), interestEmoji + " " + interestName);
+                    }
+
+                } else {
+                    makeDialogInfo("Error", "Cannot get interests list from database");
+                    Log.i("INTERESTS", task.getException().getMessage());
+                }
+            }
+        });
+    }
+
+    private void showDialogInterestsChoice() {
+        // Create dialog from layout
+        Dialog dialog = new Dialog(RegisterActivity.this);
+        dialog.setContentView(R.layout.dialog_list_multiple_choice);
+        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.rounded_all_white_smaller_radius_background));
+
+        // Change the title
+        ((TextView) dialog.findViewById(R.id.dialog_list_multiple_choice_title)).setText("Choose your interests");
+
+        // Populate interests list
+        ListView interestsViewList = dialog.findViewById(R.id.dialog_list_multiple_choice_list);
+
+        // Get all interests names from interests map
+        String[] interestsList = new String[interests.size()];
+        interests.values().toArray(interestsList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(RegisterActivity.this, android.R.layout.simple_list_item_1, interestsList);
+        interestsViewList.setAdapter(adapter);
+
+        // Show dialog
+        dialog.show();
+
+        interestsViewList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        dialog.findViewById(R.id.dialog_list_multiple_choice_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Close dialog
+                dialog.dismiss();
+            }
+        });
     }
 
     private void showDialogAvatarChoice() {
         // Create dialog from layout
         Dialog dialog = new Dialog(RegisterActivity.this);
-        dialog.setContentView(R.layout.dialog_list_choice);
+        dialog.setContentView(R.layout.dialog_list_single_choice);
         dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.rounded_all_white_smaller_radius_background));
 
         // Change the title
-        ((TextView) dialog.findViewById(R.id.dialog_list_choice_title)).setText("Choose your avatar");
+        ((TextView) dialog.findViewById(R.id.dialog_list_single_choice_title)).setText("Choose your avatar");
 
         // Populate avatar list with emojis from resources
-        ListView avatarList = dialog.findViewById(R.id.dialog_list_choice_list);
+        ListView avatarList = dialog.findViewById(R.id.dialog_list_single_choice_list);
         final String[] avatars = getResources().getStringArray(R.array.avatar_emojis);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(RegisterActivity.this, android.R.layout.simple_list_item_1, avatars);
         avatarList.setAdapter(adapter);
@@ -62,6 +167,30 @@ public class RegisterActivity extends AppCompatActivity {
                 registerAvatarEmoji.setText(avatars[position]);
                 registerAvatarBtn.setText("Change avatar");
 
+                // Close dialog
+                dialog.dismiss();
+            }
+        });
+    }
+
+    private void makeDialogInfo(String title, String description) {
+        // Create dialog from layout
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_info);
+        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.rounded_all_white_smaller_radius_background));
+
+        // Change the title
+        ((TextView) dialog.findViewById(R.id.dialog_info_title)).setText(title);
+
+        // Change description
+        ((TextView) dialog.findViewById(R.id.dialog_info_description)).setText(description);
+
+        // Show dialog
+        dialog.show();
+
+        dialog.findViewById(R.id.dialog_info_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 // Close dialog
                 dialog.dismiss();
             }
